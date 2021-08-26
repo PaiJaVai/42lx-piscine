52570863503304949ac758d53199047406748979
PaiJaVai <86362288+PaiJaVai@users.noreply.github.com>
2021-08-26 20:30:31 +0100

    Add files via upload

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiguel- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/06 12:06:59 by bmiguel-          #+#    #+#             */
+/*   Updated: 2021/08/10 12:20:23 by bmiguel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	soma(char a, char b)
+{
+	ft_putchar('0' + a / 10);
+	ft_putchar('0' + a % 10);
+	ft_putchar(' ');
+	ft_putchar('0' + b / 10);
+	ft_putchar('0' + b % 10);
+	if ((a < 98) || (b < 99))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			soma(a, b);
+			b++;
+		}
+		a++;
+	}
+}
